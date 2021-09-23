297e4bc3ff80e0e5bf16ce65a039877bfc055e82
aiosoftware <91295261+aiosoftware@users.noreply.github.com>
2021-09-23 16:58:06 -0700

    Create mongoDB.js

---
+++ mongoDB.js
@@ -0,0 +1,11 @@
+// error objects to use with React
+
+const InputErrors = {
+  
+  required : { username : 'username is required', password : 'password is required', email : 'email is required' },
+  
+  maxLength : { 20 : 'must be under 20 characters', 30 : 'must be under 30 characters', 40 : 'must be under 40 characters' },
+  
+  minLength : { 8 : 'must be over 8 characters' , 10 : 'must be over 10 characters', 12 : 'must be over 12 characters' }
+
+}
